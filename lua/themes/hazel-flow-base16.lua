099d01db0e74186c9b988b3510c14f5fa698b082
Jason Dsouza <jasmcaus@gmail.com>
2021-08-23 15:31:13 +0530

    Update base16

--- lua/themes/hazel-flow-base16.lua
+++ lua/themes/hazel-flow-base16.lua
@@ -2,20 +2,20 @@
     TODO: Remove the `#`
 --]]
 return {
-    base0A = "#e5c07b",
-    base04 = "#565c64",
-    base07 = "#c8ccd4",
-    base05 = "#abb2bf",
-    base0E = "#c678dd",
-    base0D = "#61afef",
-    base0C = "#56b6c2",
-    base0B = "#98c379",
-    base02 = "#3e4451",
-    base0F = "#be5046",
-    base03 = "#545862",
-    base08 = "#e06c75",
-    base01 = "#353b45",
     base00 = "#1e222a",
+    base01 = "#353b45",
+    base02 = "#3e4451",
+    base03 = "#545862",
+    base04 = "#565c64",
+    base05 = "#abb2bf",
+    base06 = "#b6bdca",
+    base07 = "#c8ccd4",
+    base08 = "#e5534b",
     base09 = "#d19a66",
-    base06 = "#b6bdca"
+    base0A = "#daaa3f",
+    base0B = "#8ddb8c",
+    base0C = "#96d0ff"
+    base0D = "#6cb6ff",
+    base0E = "#ab9ae1",
+    base0F = "#be5046",
 }
