8629cb24ca5270c5c3be6f72bf9104825833a372
Jason Dsouza <jasmcaus@gmail.com>
2021-08-23 15:33:20 +0530

    Add hashes back

--- lua/themes/hazel-flow-base16.lua
+++ lua/themes/hazel-flow-base16.lua
@@ -1,21 +1,18 @@
---[[
-    TODO: Remove the `#`
---]]
 return {
-    base00 = "#1e222a",
-    base01 = "#353b45",
-    base02 = "#3e4451",
-    base03 = "#545862",
-    base04 = "#565c64",
-    base05 = "#abb2bf",
-    base06 = "#b6bdca",
-    base07 = "#c8ccd4",
-    base08 = "#e5534b",
-    base09 = "#d19a66",
-    base0A = "#daaa3f",
-    base0B = "#8ddb8c",
-    base0C = "#96d0ff"
-    base0D = "#6cb6ff",
-    base0E = "#ab9ae1",
-    base0F = "#be5046",
+    base00 = "1e222a",
+    base01 = "353b45",
+    base02 = "3e4451",
+    base03 = "545862",
+    base04 = "565c64",
+    base05 = "abb2bf",
+    base06 = "b6bdca",
+    base07 = "c8ccd4",
+    base08 = "e5534b",
+    base09 = "d19a66",
+    base0A = "daaa3f",
+    base0B = "8ddb8c",
+    base0C = "96d0ff"
+    base0D = "6cb6ff",
+    base0E = "ab9ae1",
+    base0F = "be5046",
 }
